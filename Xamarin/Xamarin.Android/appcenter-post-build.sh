fef13fb4e7a948601eb976f984b6109e5a37ab07
sum0999 <sum@microsoft.com>
2018-11-23 01:43:49 +0530

    Update appcenter-post-build.sh

--- Xamarin/Xamarin.Android/appcenter-post-build.sh
+++ Xamarin/Xamarin.Android/appcenter-post-build.sh
@@ -7,3 +7,7 @@ echo "HELLP APPCENTER POST BUILD"
 echo "Creating a file">$APPCENTER_OUTPUT_DIRECTORY/NewFile
 
 [ -f $APPCENTER_OUTPUT_DIRECTORY/NewFile ] && echo "File exist" || echo "File does not exist"
+
+echo "Repro"
+
+exit 1
